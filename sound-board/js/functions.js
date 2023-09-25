fccd80dcc3c1ddbc5159d45f6477f9b83cb0b83e
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-09-25 17:16:22 -0300

    Add files via upload

---
+++ sound-board/js/functions.js
@@ -0,0 +1,26 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).volume = 0.08; //Alterar o volume dos sons.
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
