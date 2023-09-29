4a5877b031ab243e5b719b9288f032b41c7ffbaf
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-09-29 17:04:04 -0300

    Add files via upload

---
+++ content-placeholder/js/functions.js
@@ -0,0 +1,25 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1695754190419-bedddd21ccb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80" alt="" />'
+  title.innerHTML = 'Lugares para você visitar'
+  excerpt.innerHTML =
+    'Está preparado para uma viagem extraordinária?'
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/42.jpg" alt="" />'
+  name.innerHTML = 'Marcelo S.'
+  date.innerHTML = '22 de Set, 2023'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
